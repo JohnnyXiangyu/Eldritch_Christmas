c811c3719813328bb6430a9fd9fa51991438da9a
kayraigames <91762362+kayraigames@users.noreply.github.com>
2021-12-16 15:01:42 -0800

    Ai pathfinder package download

---
+++ Assets/movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    [SerializeField]
+    private float frequency = 5f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
