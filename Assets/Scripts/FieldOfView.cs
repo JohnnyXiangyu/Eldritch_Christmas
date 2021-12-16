87c1f7fb72639fa2248660cad11491f94971aebf
Cole Strain <colestrain2000@gmail.com>
2021-12-15 21:29:23 -0800

    Basic field of view

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius = 3;
+    [Range(0,360)]
+    public float viewAngle = 100;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void FixedUpdate()
+    {
+        visibleTargets.Clear();
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        foreach (Collider2D target in targetsInViewRadius)
+        {
+            Transform t = target.transform;
+
+            // Ignore self
+            if(t == this.transform)
+            {
+                continue;
+            }
+
+            Vector2 dirToTarget = (t.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector2.Distance(transform.position, t.position);
+
+                if (!Physics2D.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(t);
+                }
+            }
+        }
+    }
+
+    public Vector2 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal = true)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.z;
+        }
+        return new Vector2(-Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
