3db0714e0402e043a826b661e621659d56f002a1
Cole Strain <colestrain2000@gmail.com>
2021-12-17 11:17:08 -0800

    Finished title screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using DG.Tweening;
 
 public class TitleScreen : MonoBehaviour
@@ -19,6 +22,30 @@ public class TitleScreen : MonoBehaviour
     void ShowTitleScreen()
     {
         video.gameObject.SetActive(false);
+        background.gameObject.SetActive(true);
         background.DOFade(1f, 2f);
+        background.transform.GetChild(0).GetComponent<Text>().DOFade(1f, 2f);
+        background.transform.GetChild(1).GetComponent<Image>().DOFade(1f, 2f);
+        background.transform.GetChild(2).GetComponent<Image>().DOFade(1f, 2f);
+
+        if (Gamepad.current != null || Joystick.current != null)
+        {
+            Debug.Log(Gamepad.current.displayName);
+            GameObject.FindObjectOfType<EventSystem>().SetSelectedGameObject(background.transform.GetChild(1).gameObject);
+        }
+        else
+        {
+            GameObject.FindObjectOfType<EventSystem>().SetSelectedGameObject(null);
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
